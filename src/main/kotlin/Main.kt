57ebf929d2211c4217902c74b20c0b22b23978e5
Sagiyama <tomoyuki.sagiyama@toranoana.co.jp>
2025-02-07 15:20:32 +0900

    Modify codes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,16 @@ fun getLuckyNumber(): Int {
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    fun isLuckyNumber(number: Int): Boolean {
+        return number== 777
+    }
+
     val name = "Kotlin"
     // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
+
+    isLuckyNumber(666)
 
     for (i in 1..5) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
