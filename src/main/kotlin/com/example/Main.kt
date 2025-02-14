f422f72baf90d4a2d340798dacc1877774c9c2e5
Sagiyama <tomoyuki.sagiyama@toranoana.co.jp>
2025-02-14 13:25:33 +0900

    Actions内のライブラリのバージョンアップ

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,12 +1,8 @@
 package com.example
 
-const val LUCKY_NUMBER = 777
+
 
 fun main() {
-    fun isLuckyNumber(number: Int): Boolean {
-        return number == LUCKY_NUMBER
-    }
-
     val name = "Kotlin"
     // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
@@ -23,4 +19,10 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+}
+
+const val LUCKY_NUMBER = 777
+
+fun isLuckyNumber(number: Int): Boolean {
+    return number==LUCKY_NUMBER
 }
